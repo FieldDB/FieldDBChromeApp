481fc9e14b2d4d914a4504ef5c708a244c0c01bf
cesine <cesine@yahoo.com>
2014-01-16 11:21:07 -0500

    chrome app which shows instructions window, and the prototype running in a sandbox which is fill screenable

--- background.js
+++ background.js
@@ -1,7 +1,20 @@
 chrome.app.runtime.onLaunched.addListener(function(launchData) {
     var opt = {
-        width: 1000,
-        height: 700
+        width: 1024,
+        height: 640
+    };
+
+    chrome.app.window.create('index.html', opt, function (win) {
+        win.launchData = launchData;
+    });
+});
+
+
+chrome.app.runtime.onLaunched.addListener(function(launchData) {
+    var opt = {
+    	// frame: "none",
+        width: 505,
+        height: 600
     };
 
     chrome.app.window.create('sandbox.html', opt, function (win) {
